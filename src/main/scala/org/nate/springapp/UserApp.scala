6358f1b48b4682f0b1e6d7832403b5a7a9bd0854
natebuwalda <natebuwalda@gmail.com>
2012-03-09 10:14:34 -0600

    Nate - cleaned up app a bit

--- src/main/scala/org/nate/springapp/UserApp.scala
+++ src/main/scala/org/nate/springapp/UserApp.scala
@@ -2,14 +2,18 @@ package org.nate.springapp
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.nate.Logging
+import org.springframework.beans.factory.annotation.{Qualifier, Autowire}
 
 object SpringApp extends App with Logging {
+  
   logger.info("Starting User Load Batch Application")
   
   private val appCtx = new AnnotationConfigApplicationContext
   appCtx.scan("config")
   appCtx.refresh
 
+  if (args.length != 2) throwArgumentError
+  
 	private val runType: String = args(0)
 	private val command: String = args(1)
 
@@ -17,7 +21,7 @@ object SpringApp extends App with Logging {
     runType match {
       case "single" => appCtx.getBean("stringUserLoader", classOf[UserLoader])
       case "file" => appCtx.getBean("fileUserLoader", classOf[UserLoader])
-      case _ => throw new IllegalArgumentException("Please specify single or file entry")
+      case _ => throwArgumentError
     }
   }
 
@@ -25,4 +29,6 @@ object SpringApp extends App with Logging {
   logger.info(result)
 
   logger.info("Spring User Load Batch Application done")
+  
+  def throwArgumentError = throw new IllegalArgumentException("Please specify single or file entry and the record")
 }
